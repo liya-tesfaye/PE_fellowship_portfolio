ec428980e3d88c0583882e2803df1f8e3a961aa1
Adora Lin <adoraloin@gmail.com>
2026-06-18 13:24:11 -0400

    feat: added navbar and hobbies page

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,31 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
 
+pages = [
+    {"endpoint": "index", "name": "Home", "url": "/"},
+    {"endpoint": "hobbies", "name": "Hobbies", "url": "/hobbies"},
+]
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template(
+        "index.html",
+        title="MLH Fellow",
+        url=os.getenv("URL"),
+        pages=pages,
+    )
+
+
+@app.route("/hobbies")
+def hobbies_page():
+    return render_template(
+        "hobbies.html",
+        title="My Hobbies",
+        url=os.getenv("URL"),
+        pages=pages,
+    )
