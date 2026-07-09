781b0d4d273d5e82da7a258639c4de7bbc57a288
Liya Tesfaye <liyagebrutesfaye@gmail.com>
2026-07-09 23:45:19 +0000

    Updated redeploy-site.sh script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-tmux kill-server
-
 cd ~/PE_fellowship_portfolio || exit 1
 
 git fetch
@@ -11,5 +9,4 @@ source python3-virtualenv/bin/activate
 
 pip install -r requirements.txt
 
-tmux new-session -d -s flask \
-'cd ~/PE_fellowship_portfolio && source python3-virtualenv/bin/activate && flask run --host=0.0.0.0'
+systemctl restart myportfolio
